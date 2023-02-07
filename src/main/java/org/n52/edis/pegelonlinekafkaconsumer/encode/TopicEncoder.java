8ecb316ac9df357c05547f4f10065e12819d4f90
Sebastian Drost <s.drost@52north.org>
2023-02-07 12:43:29 +0100

    Improve topic encoding

--- src/main/java/org/n52/edis/pegelonlinekafkaconsumer/encode/TopicEncoder.java
+++ src/main/java/org/n52/edis/pegelonlinekafkaconsumer/encode/TopicEncoder.java
@@ -15,6 +15,9 @@ public class TopicEncoder {
 
     private final static String TOPIC_LEVEL_STATE_DEFAULT = "deutschland";
 
+    private static final String WHITESPACE_TOKEN = "\\s+";
+    private static final String UNDERSCORE_TOKEN = "_";
+
     public List<PegelonlineTopic> encode(PegelonlineKafkaMessage payload) {
         return payload.getTimeseries().stream().map(ts -> encode(payload, ts)).collect(Collectors.toList());
     }
@@ -22,12 +25,18 @@ public class TopicEncoder {
     public PegelonlineTopic encode(PegelonlineKafkaMessage message, KafkaTimeseries timeseries) {
         PegelonlineTopic topic = new PegelonlineTopic();
         topic.setRoot(baseTopic);
-        topic.setWater(message.getWater().getShortname());
-        topic.setState(message.getState());
-        topic.setRegion(message.getRegion());
-        topic.setAgency(message.getAgency());
+        topic.setWater(normalizeTopicLevel(message.getWater().getShortname()));
+        topic.setState(normalizeTopicLevel(message.getState()));
+        topic.setRegion(normalizeTopicLevel(message.getRegion()));
+        topic.setAgency(normalizeTopicLevel(message.getAgency()));
         topic.setUuid(message.getUuid());
-        topic.setParameter(timeseries.getLongname());
+        topic.setParameter(normalizeTopicLevel(timeseries.getShortname()));
         return topic;
+    }
+
+    private String normalizeTopicLevel(String level) {
+        level = level.replaceAll(WHITESPACE_TOKEN, UNDERSCORE_TOKEN);
+        level = level.toLowerCase();
+        return level;
     }
 }
